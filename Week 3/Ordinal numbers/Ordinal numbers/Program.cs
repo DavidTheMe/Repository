c396a299dfec05eb93a0a7e0a0e72900f9f8b297
DavidTheMe <davidweddfelt@hotmail.com>
2020-09-23 12:37:49 +0200

    mission 2 and 3 day 1 week 3

---
+++ Week 3/Ordinal numbers/Ordinal numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Ordinal_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 1; number < 11; number++)
+            {
+                Console.WriteLine($"{number}{Function(number)}");
+            }
+            
+            static string Function(int funcNumber)
+            {
+                if (funcNumber == 1)
+                    return "st";
+                else if (funcNumber == 2)
+                    return "nd";
+                else if (funcNumber == 3)
+                    return "rd";
+                else 
+                    return "th";
+            }
+
+        }
+    }
+}
---
+++ Week 3/week 3 day 1 mission 2 (a better join)/week 3 day 1 mission 2 (a better join)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace week_3_day_1_mission_2__a_better_join_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var members = new List<string> { "Leo", "Pedro", "Andrea", "Andrea"};
+            Console.WriteLine($"{JoinWithAnd(members)} are currently in the party.");
+        }
+        static string JoinWithAnd(List<string> items, bool useSerialComma = true)
+        {
+            int count = items.Count;
+
+            if (count == 0)
+                return "No people";
+            else if (count == 1)
+                return items[0];
+            else if (count == 2)
+                return $"{items[0]} and {items[1]}";
+            else if ((count == 3) && (useSerialComma == false))
+                return $"{items[0]}, {items[1]} and {items[2]}";
+            else if ((count == 3) && (useSerialComma == true))
+                return $"{items[0]}, {items[1]}, and {items[2]}";
+            else if ((count == 4) && (useSerialComma == false))
+                return $"{items[0]}, {items[1]}, {items[2]} and {items[3]}";
+            else if ((count == 4) && (useSerialComma == true))
+                return $"{items[0]}, {items[1]}, {items[2]}, and {items[3]}";
+            else
+                return "Too many people";
+        }
+    }
+}
