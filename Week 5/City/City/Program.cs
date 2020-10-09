7c9f4a6ecd06ed2d6a47d3613a2efd99b0d34659
DavidTheMe <davidweddfelt@hotmail.com>
2020-10-09 15:25:57 +0200

    city

--- Week 5/City/City/Program.cs
+++ Week 5/City/City/Program.cs
@@ -8,9 +8,11 @@ namespace City
     {
         static void DrawStuff(string[,] cordinates, int map_width, int map_height)
         {
-            for (int x = 0; x < map_width; x++)
+            Console.CursorLeft = 0;
+            Console.CursorTop = 0;
+            for (int y = 0; y < map_height; y++)
             {
-                for (int y = 0; y < map_height; y++)
+                for (int x = 0; x < map_width; x++)
                 {
                     if (cordinates[x, y] == "road")
                         Console.Write("#");
@@ -20,28 +22,54 @@ namespace City
                 Console.WriteLine();
             }
         }
-        static void GenerateRoad(string[,] cordinates, int startX, int startY, int direction, int map_width, int map_height)
+        static void GenerateRoad(string[,] cordinates, int startX, int startY, int direction)
         {
-            for (int i = 0; i < 15; i++)
+            //Variables
+
+            
+            int map_width = cordinates.GetLength(0);
+            int map_height = cordinates.GetLength(1);
+
+            for (int i = 0; i < 30; i++)
             {
-                if (startX + i < 1 || startX + i > map_width || startY + i < 1 || startY + i > map_height)
-                    break;
-                else if (direction == 0)
-                    cordinates[startX + i, startY] = "road";
+                if (direction == 0)
+                {
+                    if (startX + i < map_width)
+                    {
+                        cordinates[startX + i, startY] = "road";
+                    }
+                }
                 else if (direction == 1)
-                    cordinates[startX - i, startY] = "road";
+                {
+                    if (startX - i >= 0)
+                    {
+                        cordinates[startX - i, startY] = "road";
+                    }
+                }
                 else if (direction == 2)
-                    cordinates[startX, startY + i] = "road";
+                {
+                    if (startY + i < map_height)
+                    {
+                        cordinates[startX, startY + i] = "road";
+                    }
+                }
                 else if (direction == 3)
-                    cordinates[startX, startY - i] = "road";
+                {
+                    if (startY - i >= 0)
+                    {
+                        cordinates[startX, startY - i] = "road";
+                    }
+                }
             }
 
         }
         static void Main(string[] args)
         {
             //variables
+            Console.CursorVisible = false;
+
             int map_height = 20;
-            int map_width = 40;
+            int map_width = 80;
 
             var rand = new Random();
 
@@ -49,18 +77,18 @@ namespace City
             var cordinates = new string[map_width, map_height];
             for (int y = 0; y < map_height; y++)
             {
-                for (int x = 0; x < map_height; x++)
+                for (int x = 0; x < map_width; x++)
                 {
                     cordinates[x, y] = "empty";
                 }
             }
 
             //Generate roads
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 1000; i++)
             {
-                GenerateRoad(cordinates, rand.Next(1, map_width), rand.Next(1, map_height), rand.Next(0, 4), map_width, map_height);
+                GenerateRoad(cordinates, rand.Next(1, map_width), rand.Next(1, map_height), rand.Next(0, 4));
                 DrawStuff(cordinates, map_width, map_height);
-                Thread.Sleep(10);
+                Thread.Sleep(100);
             }
         }
     }
